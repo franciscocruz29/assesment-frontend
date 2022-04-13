d18f2d39f7e3ded66335373c9aad129e810357a6
franciscocruz29 <franciscohuertas25@gmail.com>
2022-04-13 09:19:54 -0500

    Fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Toolbar } from '@mui/material';
 import NavBar from './components/NavBar';
 import About from './pages/About';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Toolbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<ProductDetail />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
